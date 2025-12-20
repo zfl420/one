89a8299b5af72605b0845614a2e177093b889ae5
zfl420 <zfl420@outlook.com>
2025-12-20 19:27:55 +0800

    fix: 移除antd依赖，使用纯Tailwind CSS

--- src/main.tsx
+++ src/main.tsx
@@ -1,24 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { ConfigProvider } from 'antd'
-import zhCN from 'antd/locale/zh_CN'
 import App from './App'
 import './styles/index.css'
-import 'antd/dist/reset.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <ConfigProvider
-      locale={zhCN}
-      theme={{
-        token: {
-          colorPrimary: '#1890ff',
-          borderRadius: 8,
-          fontSize: 14,
-        },
-      }}
-    >
-      <App />
-    </ConfigProvider>
+    <App />
   </React.StrictMode>,
 )
